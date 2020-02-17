4b966d7dbdc7ec50083e72768510afbe0c6c82a2
Ben McKinley <1526042+joyfulflyer@users.noreply.github.com>
2020-02-16 21:16:59 -0800

    rafactor song to use hooks

--- src/Song/Song.js
+++ src/Song/Song.js
@@ -1,47 +1,37 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { get } from "axios";
 import SongList from "../SongList/SongList";
 import LineChart from "../LineChart/LineChart";
 import styles from "./Song.module.scss";
 
-class Song extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: [],
-      entries: [],
-      name: `Song Name`,
-      artist: `Artist`
-    };
-  }
+function Song(props) {
+  const [entries, setEntries] = useState([]);
+  const [open, setOpenEntries] = useState([]);
+  const [name, setName] = useState(`Song Name`);
+  const [artist, setArtist] = useState(`Artist`);
+  const { songId } = props;
+  console.log(songId);
 
-  componentDidMount() {
-    const { songId } = this.props;
+  useEffect(() => {
+    console.log(`getting ${songId}`);
     get(`/api/song/${songId}/more`)
       .then(({ data }) => {
-        this.setState({
-          name: data.name,
-          entries: data.charts,
-          artist: data.artist
-        });
+        setName(data.name);
+        setEntries(data.charts);
+        setArtist(data.artist);
       })
       .catch(err => {
         console.log(err);
       });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1 className={styles.songName}> {this.state.name} </h1>
-        <h2 className={styles.songArtist}> {this.state.artist} </h2>
-        {this.state.entries.length > 0 && (
-          <LineChart entries={this.state.entries} />
-        )}
-        <SongList entries={this.state.entries} />
-      </div>
-    );
-  }
+  }, [songId]);
+  return (
+    <div>
+      <h1 className={styles.songName}> {name} </h1>
+      <h2 className={styles.songArtist}> {artist} </h2>
+      {entries.length > 0 && <LineChart entries={entries} />}
+      <SongList entries={entries} />
+    </div>
+  );
 }
 
 export default Song;
