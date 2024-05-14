cee898d29e34954eacf3c50383aac434dfc3f57e
joyfulflyer <1526042+joyfulflyer@users.noreply.github.com>
2024-05-13 20:04:12 -0700

    We see songs

--- src/pages/ArtistDetails/ArtistDetailsPage.js
+++ src/pages/ArtistDetails/ArtistDetailsPage.js
@@ -5,16 +5,17 @@ import { useQuery } from '../../components/hooks/useQuery';
 
 const ArtistDetailsPage = () => {
 
-    const [artist, setArtist] = useState([])
-    const artistName = useQuery('artist')
+    const [songs, setSongs] = useState([])
+    const [artistName, setArtistName] = useState('')
+    const query = useQuery('artist')
 
 
     useEffect(() => {
         const fetchArtist = async () => {
             try {
                 // I'm not sure this this enpoint exists
-                const response = await axios.get(`/api/artist?artist=${artistName}`)
-                setArtist(response.data)
+                const response = await axios.get(`/api/artist?artist=${query}`)
+                setSongs(response.data)
             } catch (e) {
                 console.log(e)
             }
@@ -22,19 +23,25 @@ const ArtistDetailsPage = () => {
         fetchArtist()
     }, [])
 
+    useEffect(() => {
+        if (songs.length > 0) {
+            setArtistName(songs[0].artist)
+        }
+    }, songs)
+
 
     return (
         <div>
             <h1 className="artist-details-header">Artist Details</h1>
             <div className="artist-details">
                 <img className="artist-details__image" src="artist-image-url" alt="Artist" />
-                <h2 className="artist-details__name">Artist Name</h2>
+                <h2 className="artist-details__name">{artistName}</h2>
                 <p className="artist-details__description">Artist Description</p>
                 <h3 className="artist-details__popular-songs">Popular Songs</h3>
                 <div className="artist-details__songs">
-                    <div className="artist-details__song">Song 1</div>
-                    <div className="artist-details__song">Song 2</div>
-                    <div className="artist-details__song">Song 3</div>
+                    {songs.map((song, index) => (
+                        <div className="artist-details__song" key={index}>{song.name}</div>
+                    ))}
                 </div>
             </div>
         </div>
