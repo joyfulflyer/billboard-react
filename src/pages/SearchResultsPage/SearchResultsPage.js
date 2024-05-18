e97b5d5723e8f59ec40803f1880a495ce5d5e4f6
joyfulflyer <1526042+joyfulflyer@users.noreply.github.com>
2024-05-17 18:01:12 -0700

    seems to be working

--- src/pages/SearchResultsPage/SearchResultsPage.js
+++ src/pages/SearchResultsPage/SearchResultsPage.js
@@ -1,4 +1,4 @@
-import { useLocation } from "@reach/router";
+import { useLocation, useNavigate } from "@reach/router";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import SearchSongName from "../../SearchSongName/SearchSongName";
@@ -9,19 +9,35 @@ function SearchResultPage() {
     const [query] = useState(new URLSearchParams(useLocation().search) || 'Search')
     const [nameQuery, setNameQuery] = useState(query.get('name'))
     const [artistQuery, setArtistQuery] = useState(query.get('artist'))
+    const navigate = useNavigate()
 
-    const sendSearch = async (songName, artist) => {
-        var url = '/api/search?'
+    const generateSearchParams = (songName, baseUrl, artist, searchType) => {
+        var url = baseUrl;
         if (songName) {
-            url = url.concat(`name=${songName}`)
+            url = url.concat(`name=${songName}`);
         }
 
         if (artist) {
             if (songName) {
-                url = url.concat('&')
+                url = url.concat('&');
             }
-            url = url.concat(`artist=${artist}`)
+            url = url.concat(`artist=${artist}`);
         }
+        if (songName && artist && searchType) {
+            if (searchType != 'AND' && searchType != 'OR') {
+                throw new Error('Invalid search type');
+            }
+            url = url.concat(`&searchType=${searchType}`);
+        }
+        return url;
+    }
+
+
+    // searchType is generally 'AND' or 'OR'
+    // I could pass the query through but I'm reconstructing it here for now
+    const sendSearch = async (songName, artist, searchType=undefined) => {
+        var url = '/api/search?'
+        url = generateSearchParams(songName, url, artist, searchType);
         try {
             const result = await axios.get(url)
             return (result.data)
@@ -47,12 +63,17 @@ function SearchResultPage() {
         }
     }
 
-    const onClickAnd = () => {
-        // TODO
-    }
-
-    const onClickOr = () => {
-        // TODO
+    const onClickAnd = (searchType) => {
+        debugger
+        sendSearch(nameQuery, artistQuery, searchType).then((result) => {
+            debugger
+            const baseUrl = '/search?'
+            // this is dangerous, there is potential race condition here
+            navigate(generateSearchParams(nameQuery, baseUrl, artistQuery, searchType))
+            setSearchResults(result)
+        }).catch(err => {
+        console.log(err)
+        })
     }
 
     // I should only re-render the bottom half but can do a full page refresh for now
@@ -73,8 +94,8 @@ function SearchResultPage() {
                 </div>
                 <div>
                     <form>
-                        <button type="submit" onClick={e => onClickOr(e.target.value)}>OR</button>
-                        <button type="submit" onClick={e => onClickAnd(e.target.value)}>AND</button>
+                        <button type="button" onClick={e => onClickAnd(e.target.innerText)}>OR</button>
+                        <button type="button" onClick={e => onClickAnd(e.target.innerText)}>AND</button>
                     </form>
                 </div>
             </div>
@@ -84,6 +105,7 @@ function SearchResultPage() {
     return (
         <div>{searchForm()}{showSongNames()}</div>
     )
+
 }
 
 export default SearchResultPage;
