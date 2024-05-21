ef79b2527f696bfa57d8e837d4bcc8768a523577
joyfulflyer <1526042+joyfulflyer@users.noreply.github.com>
2024-05-20 18:46:26 -0700

    Update required removing the tests

--- src/ChartEntry/ChartEntry.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ChartEntry from "./ChartEntry";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<ChartEntry />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/Song/Song.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Song from "./Song";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Song />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
