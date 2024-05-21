2eeca9115f4ab33a78a4c1999ec029fecb824a2e
Ben McKinley <1526042+joyfulflyer@users.noreply.github.com>
2024-05-20 19:48:20 -0700

    Update router (#16)
    Update router to use react router

--- src/Song/Song.js
+++ src/Song/Song.js
@@ -1,47 +1,38 @@
-import React, { Component } from "react";
 import axios from "axios";
-import SongList from "../SongList/SongList";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import LineChart from "../LineChart/LineChart";
+import SongList from "../SongList/SongList";
 import styles from "./Song.module.scss";
 
-class Song extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: [],
-      entries: [],
-      name: `Song Name`,
-      artist: `Artist`
-    };
-  }
+function Song(props) {
+  const [entries, setEntries] = useState([]);
+  const [name, setName] = useState(`Song Name`);
+  const [artist, setArtist] = useState(`Artist`);
+  const { songId } = useParams();
 
-  componentDidMount() {
-    const { songId } = this.props;
+  useEffect(() => {
     axios.get(`/api/song/${songId}`)
       .then(({ data }) => {
-        this.setState({
-          name: data.name,
-          entries: data.charts,
-          artist: data.artist
-        });
+        setName(data.name);
+        setEntries(data.charts);
+        setArtist(data.artist);
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  }, [songId]);
 
-  render() {
-    return (
-      <div>
-        <h1 className={styles.songName}> {this.state.name} </h1>
-        <h2 className={styles.songArtist}> {this.state.artist} </h2>
-        {this.state.entries.length > 0 && (
-          <LineChart entries={this.state.entries} />
-        )}
-        <SongList entries={this.state.entries} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1 className={styles.songName}> {name} </h1>
+      <h2 className={styles.songArtist}> {artist} </h2>
+      {entries.length > 0 && (
+        <LineChart entries={entries} />
+      )}
+      <SongList entries={entries} />
+    </div>
+  );
 }
 
 export default Song;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Router } from "@reach/router";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styles from "./App.module.scss";
+import Header from "./Header/Header";
 import Song from "./Song/Song";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <Router>
-    <App path="/">
-      <Song path="song/:songId" />
-    </App>
-  </Router>,
-  document.getElementById("root")
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <div className={styles.App}>
+      <Header />
+      <Routes>
+        <Route element={<Song />} path="song/:songId" />
+      </Routes>
+    </div>
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
